a1d7d82415aa248e1e016bba5c06b3786fae562a
Bernard Moy <bernardmoy3@gmail.com>
2025-12-07 13:49:30 +0000

    Day 7

---
+++ day7/code.py
@@ -0,0 +1,29 @@
+with open('input.txt', 'r') as f: 
+    lines = f.read().split('\n')[:-1]
+from collections import defaultdict
+
+# Given: The ^ and . must alternate, and no ^ can be on the edges. 
+initial = lines[0].find('S')
+times = [0]*len(lines[0])
+times[initial] = 1 
+s = set([initial]) 
+ans = 0 
+tree = defaultdict(list) 
+for i in range(1, len(lines)): 
+    for j in range(len(lines[i])): 
+        if lines[i][j] == '^':
+            # Split beams into two  
+            if j in s: 
+                s.remove(j)
+                s.add(j-1) 
+                s.add(j+1)
+                ans += 1 
+                tree[(i-1,j)].append((i+1, j-1))
+                tree[(i-1,j)].append((i+1, j+1))
+                times[j-1] += times[j]   # Prefix addition 
+                times[j+1] += times[j] 
+                times[j] = 0 
+
+print(ans)
+
+print(sum(times))
